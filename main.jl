51d3c68c80369a85d891b8521c16e5c4c7f65ed0
Ren634 <al20065@shibaura-it.ac.jp>
2021-02-04 17:38:48 +0900

    fix call function

--- main.jl
+++ main.jl
@@ -1,34 +1,49 @@
 mutable struct Variable
     data
+    creator
     function Variable(data)
-        new(data)
+        new(data,nothing)
     end
 end
 
-mutable struct add
+mutable struct Add
     inputs
     outputs
-    function add(x0,x1)
-        self = new()
-        inputs = [x0,x1] #need fixing to generally take Variable 2/2
-        outputs = call(self,x0,x1)
-    end
 end
 
-function call(func,inputs :: Variable ...)
-    xs = [input.data for input in inputs]
-    println(xs)
-    output =  [Variable(output) for output in forward(func,xs...)]
-    return output[1]
+function add(x0,x1)
+    self = Add(nothing,nothing)
+    call(self,x0,x1)
 end
 
-function forward(func :: add,x0 :: Float64,x1::Float64)
+function forward(func :: Add,x0 :: Array{Float64},x1::Array{Float64})
     y = x0 + x1
     return y
 end
 
+function call(func,inputs :: Variable ...)
+    xs = [input.data for input in inputs]
+    ys = forward(func,xs...)
+    if (!isa(ys,Array{Any}))
+        ys = (ys)
+    end
+    outputs = [Variable(y) for y in ys]
+    println(outputs)
+    func.inputs = inputs
+    func.outputs = [output for output in outputs]
+    for output in outputs
+        output.creator = func
+    end
+    if (length(outputs) > 1)
+        return outputs
+    else 
+        return outputs[1]
+    end
+end
 
-x = Variable(1.0)
-y = Variable(2.0)
+
+x = Variable([1.0])
+y = Variable([2.0])
 z = add(x,y)
 println(z.data)
+println(z.creator)
